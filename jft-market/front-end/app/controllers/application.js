8412b76a144d890298fcf01df163408ce06a10ff
Ashutosh Ojha <ashutosh.ojha@appdirect.com>
2017-04-20 18:07:08 +0530

    login auth with ember

--- jft-market/front-end/app/controllers/application.js
+++ jft-market/front-end/app/controllers/application.js
@@ -3,15 +3,17 @@ import Constant from 'jft-sale/constant';
 import sweetAlert from 'ember-sweetalert';
 
 export default Ember.Controller.extend({
-  
-  isAdmin: false  ,
+
+  session: Ember.inject.service('session'),
+
+  isAdmin: false ,
 
   isUser: true,
-  
+
   isLogin: false,
 
   Constant: Constant,
-  
+
   cart: localStorage.getItem('cart') || 0,
 
   actions: {
@@ -19,6 +21,7 @@ export default Ember.Controller.extend({
 
       //console.info(localStorage, 'ppll');
       //localStorage.setItem("ddd", "ddddddddd")
+      this.get('session').invalidate();
       this.set('isLogin', false);
       this.set('isAdmin', false);
       this.set('isUser', true);
